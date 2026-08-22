bcda73f6f0dd41b6455cd00d16ae0448f95eabda
lains <ling3212045218@gmail.com>
2026-08-22 11:17:21 +0800

    fix: route api requests through functions/api

--- functions/[[route]].ts
+++
@@ -1,5 +0,0 @@
-import { handle } from 'hono/cloudflare-pages';
-import app from '../packages/server/src/index';
-
-// 将已有的 Hono 后端直接挂载为 Cloudflare Pages Functions
-export const onRequest = handle(app);
